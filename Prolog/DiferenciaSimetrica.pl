7706923da697396267c3fb4123e80f6a78bd443b
Leonardo Pinzon <leonardo3103@gmail.com>
2018-11-24 12:28:57 -0400

    ejercicios de prolog 24/11/2018

---
+++ Prolog/DiferenciaSimetrica.pl
@@ -0,0 +1,12 @@
+difSimetrica(A,B,R):-dif(A,B,R1),dif(B,A,R2),append(R1,R2,R).
+
+dif([],_,[]):-!.
+dif(A,[],A):-!.
+dif([Ae|A],B,R):-pertenece(Ae,B),dif(A,B,R),!.
+dif([Ae|A],B,[Ae|R]):-dif(A,B,R).
+
+% recuerden que en lugar de crear el pertenece pueden usar el%
+% member(Ae,B) que hace exactamente lo mismo revisa si un elemento Ae%
+% pertenece a una lista B%
+pertenece(Ae,[Ae|_]):-true,!.
+pertenece(Ae,[_|B]):-pertenece(Ae,B).
---
+++ Prolog/subLista.pl
@@ -0,0 +1,4 @@
+%comentario%
+% subLista([1,2,3],[1,2,4,5,6,1,2,3]). = true ya que consigue una lista%
+% L1,L2 que hacen que concatenada con A den B como no me importa%
+subLista(A, B):-append(_,L2, B),append(A,_, L2).
